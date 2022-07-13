4d5ff411f11cdc7a3ad08832418e587d35203d3b
nolancfar <106340055+nolancfar@users.noreply.github.com>
2022-07-13 09:57:55 -0700

    Update counter.m
    I think it's finally fully working

--- counter.m
+++ counter.m
@@ -27,38 +27,38 @@ for i = 1:length(fileNames)
         for k=1:length(ID_uniqueList)
             drop_indx = tempCanData.(['ID' ID_uniqueList{k}]).msgDropouts;
             if any(drop_indx)
-                    fprintf(outputLog,'    %d dropouts were identified for CAN ID: %s\n',num2str(sum(drop_indx)),ID_uniqueList{k});
+                fprintf(outputLog,'    %s dropouts were identified for CAN ID: %s\n',num2str(sum(drop_indx)),ID_uniqueList{k});
             end
         end
 
         %determine and print error counts
         if errors
-            totalErrorCount = height(tempErrorTable);
+            totalErrorCount = length(tempErrorTable.TimestampEpoch);
             fprintf(outputLog,'\n    Total Number of Errors: %d\n',totalErrorCount);
 
             if isfield(tempErrorData,'error0')
-                error0count = length(tempErrorData.error0);
-                fprintf(outputLog,'     Number of Unknown Errors: %d\n',error0count);
+                error0count = length(tempErrorData.error0.TimestampEpoch);
+                fprintf(outputLog,'       Number of Unknown Errors: %d\n',error0count);
             end
             if isfield(tempErrorData,'error1')
-                error1count = length(tempErrorData.error1);
-                fprintf(outputLog,'     Number of Bit Errors: %d\n',error1count);
+                error1count = length(tempErrorData.error1.TimestampEpoch);
+                fprintf(outputLog,'       Number of Bit Errors: %d\n',error1count);
             end
             if isfield(tempErrorData,'error2')
-                error2count = length(tempErrorData.error2);
-                fprintf(outputLog,'     Number of Form Errors: %d\n',error2count);
+                error2count = length(tempErrorData.error2.TimestampEpoch);
+                fprintf(outputLog,'       Number of Form Errors: %d\n',error2count);
             end
             if isfield(tempErrorData,'error3')
-                error3count = length(tempErrorData.error3);
-                fprintf(outputLog,'     Number of Bit-Stuffing Errors: %d\n',error3count);
+                error3count = length(tempErrorData.error3.TimestampEpoch);
+                fprintf(outputLog,'       Number of Bit-Stuffing Errors: %d\n',error3count);
             end
             if isfield(tempErrorData,'error4')
-                error4count = length(tempErrorData.error4);
-                fprintf(outputLog,'     Number of CRC Errors: %d\n',error4count);
+                error4count = length(tempErrorData.error4.TimestampEpoch);
+                fprintf(outputLog,'       Number of CRC Errors: %d\n',error4count);
             end
             if isfield(tempErrorData,'error5')
-                error5count = length(tempErrorData.error5);
-                fprintf(outputLog,'     Number of ACK Errors: %d\n',error5count);
+                error5count = length(tempErrorData.error5.TimestampEpoch);
+                fprintf(outputLog,'       Number of ACK Errors: %d\n',error5count);
             end
         else
             fprintf(outputLog,'\n    Errors not present in this log');
@@ -66,6 +66,8 @@ for i = 1:length(fileNames)
 
         fprintf(outputLog,'\n\n\n');
         disp([fileNames(i).name ' Bus ' num2str(busChannel) ' complete'])
+
+        clear tempErrorData tempErrorTable tempCanData tempCanTable ID_uniqueList drop_indx runtime totalErrorCount
     end
 end
 
